276d81c47d57137e1c04b969ced995df20ecff0e
FritzFak <105308607+FritzFak@users.noreply.github.com>
2022-06-04 16:02:55 +0300

    Update minimum.java

--- minimum.java
+++ minimum.java
@@ -1,22 +1,22 @@
-public class Main {
-    public static void main(String[] args) {
-        int[] array = new int[] { 64, 42, 73, 41, 31, 53, 16, 24, 57, 42, 74, 55, 36};
-        //пусть есть массив целых чисел
-        //найти минимальный элемент - найти его значения и его позицию внутри массива
-        int minValue = array[0];
-        int minIndex = 0;
-
-        for (int i = 1; i < array.lenght; i++) {
-            if (array[i] < minValue) {
-                minValue = array[1];
-                minIndex = i;
+public class Minimum {
+        public static void main(String[] args) {
+            int[] array = new int[] { 64, 42, 73, 41, 31, 53, 16, 24, 57, 42, 74, 55, 36};
+            //пусть есть массив целых чисел
+            //найти минимальный элемент - найти его значения и его позицию внутри массива
+            int minValue = array[0];
+            int minIndex = 0;
+    
+            for (int i = 1; i < array.length; i++) {
+                if (array[i] < minValue) {
+                    minValue = array[1];
+                    minIndex = i;
+                }
             }
+            System.out.println("min value: " + minValue);
+            extracted(minIndex);
         }
-        System.out.println("min value: " + minValue);
-        extracted(minIndex);
+    
+        private static void extracted(int minIndex) {
+            System.out.println("min index: " + minIndex);
+        }
     }
-
-    private static void extracted(int minIndex) {
-        System.out.println("min index: " + minIndex);
-    }
-}
